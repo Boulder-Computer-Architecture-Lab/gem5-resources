e16e75f49a71772ca75084242d317fcc4164a74e
PSCurlin <phaedra.curlin@outlook.fr>
2025-11-25 04:36:01 +0000

    restore after_boot.sh

--- gapbs/files/x86/after_boot.sh
+++ gapbs/files/x86/after_boot.sh
@@ -16,12 +16,42 @@
 printf "In after_boot.sh...\n"
 gem5-bridge hypercall 2
 
-printf "Starting gem5 init... trying to read run script file via readfile.\n"
-gem5-bridge readfile > /home/gem5/gapbs/script
-printf "Running script from gem5-bridge stored in /home/gem5/gapbs\n"
-chmod 755 /home/gem5/gapbs/script
-hypercall 2
-/home/gem5/gapbs/script
-printf "Done running script from gem5-bridge, exiting.\n"
-rm -f /home/gem5/gapbs/script
-gem5-bridge hypercall 3
+# Read /proc/cmdline and parse options
+
+cmdline=$(cat /proc/cmdline)
+interactive=false
+IGNORE_M5=0
+if [[ $cmdline == *"interactive"* ]]; then
+    interactive=true
+fi
+
+printf "Interactive mode: $interactive\n"
+
+if [[ $interactive == true ]]; then
+    printf "Interactive mode enabled, dropping to shell."
+    /bin/bash
+else
+    # Try to read the file from the host when running with gem5
+    if ! [ -z $IGNORE_M5 ]; then
+        printf "Starting gem5 init... trying to read run script file via readfile.\n"
+        if ! gem5-bridge readfile > /tmp/script; then
+            printf "Failed to run gem5-bridge readfile, exiting!\n"
+            rm -f /tmp/script
+            # If we can't read the script exit the simulation. If we cannot exit the
+            # simulation, this probably means that we are running in QEMU. So, ignore
+            # future calls to gem5-bridge.
+            if ! gem5-bridge exit; then
+                # Useful for booting the disk image in (e.g.,) qemu for debugging
+                printf "gem5-bridge exit failed, dropping to shell.\n"
+                IGNORE_M5=1 /bin/bash
+            fi
+        else
+            printf "Running script from gem5-bridge stored in /tmp/script\n"
+            chmod 755 /tmp/script
+            /tmp/script
+            printf "Done running script from gem5-bridge, exiting.\n"
+            rm -f /tmp/script
+            gem5-bridge hypercall 3
+        fi
+    fi
+fi
